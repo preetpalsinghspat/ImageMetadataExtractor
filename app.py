920586bc1f16ff9b12da8e6dcdb8bbeb6f6e7778
preetpalsinghspat <preetpalsinghspat@gmail.com>
2022-09-09 12:36:41 +0530

    Initial commit

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
     if request.method == 'POST':
         image = request.files['image']
         extension = image.filename.split('.')[-1]
